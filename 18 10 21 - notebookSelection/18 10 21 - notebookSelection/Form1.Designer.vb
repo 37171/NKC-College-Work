05b4954de6166f25005df52306c7815ad33ad196
37171 <92715797+37171@users.noreply.github.com>
2021-10-18 09:34:09 +0100

    Add files via upload
    Task from Class Notebook (Event Driven Programming > SELECTION; "Create a program which get the user’s age. If over 18, display a message that states that they can vote - if not, calculate how many years until they can vote and display that message.") completed.

---
+++ 18 10 21 - notebookSelection/18 10 21 - notebookSelection/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class voteAge
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.inputAgeBox = New System.Windows.Forms.TextBox()
+        Me.inputAgeTest = New System.Windows.Forms.Button()
+        Me.outputAgeTest = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'inputAgeBox
+        '
+        Me.inputAgeBox.Location = New System.Drawing.Point(167, 12)
+        Me.inputAgeBox.Name = "inputAgeBox"
+        Me.inputAgeBox.Size = New System.Drawing.Size(100, 23)
+        Me.inputAgeBox.TabIndex = 0
+        '
+        'inputAgeTest
+        '
+        Me.inputAgeTest.Location = New System.Drawing.Point(12, 12)
+        Me.inputAgeTest.Name = "inputAgeTest"
+        Me.inputAgeTest.Size = New System.Drawing.Size(149, 23)
+        Me.inputAgeTest.TabIndex = 1
+        Me.inputAgeTest.Text = "Am I old enough to vote?"
+        Me.inputAgeTest.UseVisualStyleBackColor = True
+        '
+        'outputAgeTest
+        '
+        Me.outputAgeTest.AutoSize = True
+        Me.outputAgeTest.Location = New System.Drawing.Point(12, 38)
+        Me.outputAgeTest.Name = "outputAgeTest"
+        Me.outputAgeTest.Size = New System.Drawing.Size(244, 15)
+        Me.outputAgeTest.TabIndex = 2
+        Me.outputAgeTest.Text = "Enter your age and hit the button to find out."
+        '
+        'voteAge
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(280, 65)
+        Me.Controls.Add(Me.outputAgeTest)
+        Me.Controls.Add(Me.inputAgeTest)
+        Me.Controls.Add(Me.inputAgeBox)
+        Me.Name = "voteAge"
+        Me.Text = "Voting Age Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents inputAgeBox As TextBox
+    Friend WithEvents inputAgeTest As Button
+    Friend WithEvents outputAgeTest As Label
+End Class
---
+++ 18 10 21 - notebookSelection/18 10 21 - notebookSelection/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class voteAge
+    Private Sub inputAgeTest_Click(sender As Object, e As EventArgs) Handles inputAgeTest.Click
+        Dim age As Integer
+        Try
+            age = inputAgeBox.Text
+            outputAgeTest.ForeColor = Color.FromArgb(0, 0, 0)
+            If age >= 18 Then
+                outputAgeTest.Text = "You are old enough to vote!"
+            Else
+                age = 18 - age
+                outputAgeTest.Text = "You can vote in " + Convert.ToString(age) + " years."
+            End If
+        Catch ex As System.InvalidCastException
+            outputAgeTest.Text = "ERROR: Please enter a valid number."
+            outputAgeTest.ForeColor = Color.FromArgb(230, 0, 0)
+        End Try
+    End Sub
+
+End Class
